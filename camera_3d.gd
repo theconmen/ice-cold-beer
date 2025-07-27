f5e60392df742cb79308b3aacb63f34a2cb43f03
theconmen <ckayoung22@gmail.com>
2025-07-27 17:30:48 -0400

    Testing physics collisions

---
+++ camera_3d.gd
@@ -0,0 +1,5 @@
+extends Camera3D
+
+#position = -0.44, 1.882, 0.002
+#rotation = -9.8, -90, 0.0
+#FOV = 90.6
---
+++ game_board.gd
@@ -0,0 +1,5 @@
+extends CSGCombiner3D
+
+
+func _physics_process(delta: float) -> void:
+	self.use_collision = true
